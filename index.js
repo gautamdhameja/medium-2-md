1bca945da7c6dd4275859b98669002752346b27a
Gautam Dhameja <gautamdhameja@users.noreply.github.com>
2018-12-02 19:03:41 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+'use strict';
+
+const program = require('commander');
+const workflow = require('./lib/workflow');
+
+program
+    .version('0.1.0')
+    .command('convert [inputDirectory]')
+    .option('-d, --drafts', 'Convert drafts too.')
+    .option('-f, --frontMatter','Add front-matter.')
+    .action(workflow);
+
+program.parse(process.argv);
+
+// (
+//     () => {
+//         workflow('D:\\Downloads\\medium-export\\posts', 'D:\\Downloads\\medium-export\\md');
+//     }
+// )()
